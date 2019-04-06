0d7d586a126e4be9fb1008e318152b700bf747cd
Unknown <marcos.alvarenga@10pines.com>
2019-04-06 17:05:04 -0300

    Initializes context for EventeandoApp

---
+++ src/main/kotlin/edu/unq/desapp/eventeando/EventeandoApplication.kt
@@ -0,0 +1,11 @@
+package edu.unq.desapp.eventeando
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventeandoApplication
+
+fun main(args: Array<String>) {
+    runApplication<EventeandoApplication>(*args)
+}
