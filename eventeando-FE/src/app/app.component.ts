9b1a673a999ea2764d0f17138129d8f04305f5ff
Unknown <marcos.alvarenga@10pines.com>
2019-04-16 21:24:49 -0300

    add service test

--- eventeando-FE/src/app/app.component.ts
+++ eventeando-FE/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ApiService} from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Arma tu evento!';
+
+    constructor(private apiService: ApiService) {}
+
+    ping() {
+        this.apiService.get('ping').subscribe(response => console.log(response));
+    }
 }
