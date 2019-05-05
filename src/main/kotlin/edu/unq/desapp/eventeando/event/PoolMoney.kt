c5bb8b1b0f849e2a778a4f278bb411f1c5fe0daf
Unknown <marcos.alvarenga@10pines.com>
2019-05-05 12:35:16 -0300

    Adds methods documentation

--- src/main/kotlin/edu/unq/desapp/eventeando/event/PoolMoney.kt
+++ src/main/kotlin/edu/unq/desapp/eventeando/event/PoolMoney.kt
@@ -8,18 +8,31 @@ import java.time.LocalDate
  * Models a pool of money, thats have.....
  */
 class PoolMoney(date: LocalDate) : Event(date) {
+
+    /**
+     * TODO
+     */
     fun costPerConfirmedGuest(): Int{
         return totalCost() / confirmedGuests().size
     }
 
+    /**
+     * TODO
+     */
     fun balanceOf(guest: Guest): Int{
-        return totalSpendsOf(guest)-this.costPerConfirmedGuest()
+        return totalSpendsOf(guest) - this.costPerConfirmedGuest()
     }
 
+    /**
+     * TODO
+     */
     fun spendsOf(guest: Guest):List<Spending>{
         return spendings.filter{ spending -> spending.isFrom(guest)}
     }
 
+    /**
+     * TODO
+     */
     fun totalSpendsOf(guest: Guest): Int{
         return spendsOf(guest).fold(0){ total, gasto -> total + gasto.cost}
     }
--- src/main/kotlin/edu/unq/desapp/eventeando/template/Template.kt
+++ src/main/kotlin/edu/unq/desapp/eventeando/template/Template.kt
@@ -3,15 +3,21 @@ package edu.unq.desapp.eventeando.template
 import edu.unq.desapp.eventeando.element.Element
 
 /**
- *
+ * TODO
  */
 class Template(val title: String, val description: String) {
     private val elements:  MutableList<Element> = mutableListOf()
 
+    /**
+     * TODO
+     */
     fun add(element: Element){
         elements.add(element)
     }
 
+    /**
+     * TODO
+     */
     fun costPerPerson():Double{
         return elements.fold(0.00){ total, element -> element.proportionPerPerson + total}
     }
