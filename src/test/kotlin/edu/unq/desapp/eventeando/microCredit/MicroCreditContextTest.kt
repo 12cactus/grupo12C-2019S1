775df839e42f912371c389505f7196b5e8b02cdc
sirDemian <sirdemian@gmail.com>
2019-05-05 18:23:27 -0300

    delete empty tests

--- src/test/kotlin/edu/unq/desapp/eventeando/microCredit/MicroCreditContextTest.kt
+++
@@ -1,9 +0,0 @@
-package edu.unq.desapp.eventeando.microCredit
-
-import ar.com.dgarcia.javaspec.api.contexts.TestContext
-import java.util.function.Supplier
-
-interface MicroCreditContextTest: TestContext {
-    fun microCredit(): MicroCreditTest
-    fun microCredit(supplier: Supplier<MicroCreditTest>)
-}
