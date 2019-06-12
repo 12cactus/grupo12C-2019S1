6eb7b75639fd8b724467f382dcafa0cbf2442ae5
Unknown <marcos.alvarenga@10pines.com>
2019-06-12 12:14:35 -0300

    Removes deprecated classes

--- src/main/kotlin/edu/unq/desapp/eventeando/element/Element.kt
+++
@@ -1,6 +0,0 @@
-package edu.unq.desapp.eventeando.element
-
-/**
- * Model things that can be added to an event and have a cost and description
- */
-class Element (val description:String, val proportionPerPerson: Double )
