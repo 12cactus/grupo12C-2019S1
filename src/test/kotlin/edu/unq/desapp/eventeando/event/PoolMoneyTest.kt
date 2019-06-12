5c3b25bd4638af05266a8365b7235f5541811472
Unknown <marcos.alvarenga@10pines.com>
2019-06-12 16:34:34 -0300

    Fix javaSpec version to use kotlinSpec

--- src/test/kotlin/edu/unq/desapp/eventeando/event/PoolMoneyTest.kt
+++ src/test/kotlin/edu/unq/desapp/eventeando/event/PoolMoneyTest.kt
@@ -17,22 +17,21 @@ import java.util.function.Supplier
 class PoolMoneyTest : KotlinSpec() {
 
     private val today = LocalDate.now()
-    private val weekAgo = today.minusDays(7)
     private val nextWeek = today.plusDays(7)
 
     override fun define() {
         describe("Given a poolMoney") {
             val organizers by let { mutableListOf(User("an organizer")) }
-            val poolMoneyDate by let<LocalDate>()
-            val limitDateToConfirm by let<LocalDate>()
+            val poolMoneyDate by let {nextWeek}
+            val limitDateToConfirm by let{nextWeek}
             val manualConfirmedGuests by let<Int>()
-            val eventExpenseCalculator by let<EventExpenseCalculator?> { null }
-            val guests by let<MutableList<User>>()
+            val eventExpenseCalculator by let<EventExpenseCalculator?>()
+            val guests by let{ mutableListOf<User>()}
             val eventExpenses by let<MutableList<EventExpense>>()
             val poolMoney by let<PoolMoney>()
 
             poolMoney {
-                PoolMoney(organizers,
+                PoolMoney(organizers(),
                         poolMoneyDate(),
                         limitDateToConfirm(),
                         manualConfirmedGuests(),
@@ -41,37 +40,27 @@ class PoolMoneyTest : KotlinSpec() {
                         eventExpenses())
             }
 
-            context().poolMoney(Supplier {
-                PoolMoney(context().organizers(),
-                        context().poolMoneyDate(),
-                        context().limitDateToConfirm(),
-                        manualConfirmedGuests,
-                        context().eventExpenseCalculator(),
-                        context().guests(),
-                        context().eventExpenses())
-            })
-
             describe("when the pool money is a buy first mode") {
-                context().eventExpenseCalculator(Supplier { null })
+                eventExpenseCalculator{null}
 
                 describe("#costPerConfirmedGuest") {
                     describe("with an event expense that represents the total cost of what was spent") {
-                        context().expense(Supplier { GeneralExpense("Gasto total", 1750.90) })
-                        context().eventExpenses(Supplier { mutableListOf(context().expense()) })
+                        val expense by let { GeneralExpense("Gasto total", 1750.90) }
+                        eventExpenses { mutableListOf(expense()) }
 
                         describe("with some manual confirmed guests") {
-                            manualConfirmedGuests = 10
+                            manualConfirmedGuests{10}
 
                             it("Returns the exact cost per confirmed Guest") {
-                                Assertions.assertThat(context().poolMoney().costPerPerson()).isEqualTo(176.0)
+                                Assertions.assertThat(poolMoney().costPerPerson()).isEqualTo(176.0)
                             }
                         }
 
                         describe("with no manually confirmed guest") {
-                            manualConfirmedGuests = 0
+                            manualConfirmedGuests {0}
 
                             itThrows(DivideByZeroException::class.java, "throws an error because ManualConfirmedGuests must be higher than zero",
-                                    { context().poolMoney().costPerPerson() },
+                                    { poolMoney().costPerPerson() },
                                     { e -> Assertions.assertThat(e).hasMessage("Cannot calculate. ManualConfirmedGuests must be higher than zero") }
                             )
                         }
